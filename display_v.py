6698e90dd0b997ad47bc39426b891818ce05e937
Ariel Lindemann <ariel.lindemann@gmail.com>
2020-03-31 23:50:18 +0200

    Can now plot based on series of inputs

--- display_v.py
+++ display_v.py
@@ -2,24 +2,40 @@ import matplotlib.pyplot as plt
 import vehicle
 import math
 
-def show_car(car, gas = 0, brake = 0, st_wheel_chg = 0, total_time = 100, t_s = 1):
-    
-    pos_x = 0
-    pos_y = 0
+def next_pos(car, pos = [0, 0], inputs = [0, 0, 0],  t_s = 1):
+    ''' Returns coordinates of Car after specified timestep based on given inputs'''
+
+    pos_x = pos[0]
+    pos_y = pos[1]
+
+    gas, brake, st_wheel_chg = inputs
+
+    car.take_inputs(gas, brake, st_wheel_chg)
+    car.update(timestep = t_s)
+
+    pos_x += car.x_p
+    pos_y += car.y_p
+
+    return pos_x, pos_y
+
+
+def get_trajectory(car, input_series, starting_pos = [0, 0], total_time = 100,  t_s = 1):
+    ''' Returns series of posisions based on a series of inputs'''
 
     all_x = []
     all_y = []
 
+    pos_x = starting_pos[0]
+    pos_y = starting_pos[1]
+
     plt.axes(aspect = 'equal')
 
-    car.take_inputs(gas, brake, st_wheel_chg)
-
     for i in range(total_time):
-        car.update()
-        car.take_inputs(st_wheel_chg= 0)
 
-        pos_x += car.x_p
-        pos_y += car.y_p
+        if len(input_series) >= i:
+            gas, brake, st_wheel_chg = input_series.pop(0)
+
+        pos_x, pos_y = next_pos(car, [pos_x, pos_y], [gas, brake, st_wheel_chg], t_s)
 
         print(pos_x, pos_y)
         all_x.append(pos_x)
@@ -27,15 +43,15 @@ def show_car(car, gas = 0, brake = 0, st_wheel_chg = 0, total_time = 100, t_s = 
 
     plt.plot(all_x,all_y, '.')
     plt.show()
-
+    
+    return all_x, all_y
     
 car = vehicle.Car()
 
 gas_pedal = 1
 brake_pedal = 0
-st_chg = math.radians(45)
+st_chg = math.radians(0.001)
 total_time = 100
 timestep = 1
 
-
-show_car(car, gas_pedal, brake_pedal, st_chg, total_time, timestep)
+get_trajectory(car, [[gas_pedal, brake_pedal, st_chg]], [0,0] , total_time, timestep)
