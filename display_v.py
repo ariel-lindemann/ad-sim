5824f33a63ed608240cd78bdc3f1c38344e3e466
Ariel Lindemann <ariel.lindemann@gmail.com>
2020-04-01 18:34:18 +0200

    updated demo to work with file

--- display_v.py
+++ display_v.py
@@ -1,6 +1,5 @@
 import matplotlib.pyplot as plt
 import vehicle
-import math
 
 
 def display_trajectory(car, input_series, starting_pos=[0, 0], total_time=100,  t_s=1):
@@ -13,10 +12,9 @@ def display_trajectory(car, input_series, starting_pos=[0, 0], total_time=100,  
 
 car = vehicle.Car()
 
-gas_pedal = 1
-brake_pedal = 0
-st_chg = math.radians(0.001)
 total_time = 100
 timestep = 1
 
-display_trajectory(car, [[gas_pedal, brake_pedal, st_chg]], [0, 0], total_time, timestep)
+inputs_file = 'inputs.csv'
+
+display_trajectory(car, inputs_file, [0, 0], total_time, timestep)
