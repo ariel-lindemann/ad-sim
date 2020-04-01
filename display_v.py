9725094e30733e70ebdc504f1bfbad92eedcf078
Ariel Lindemann <ariel.lindemann@gmail.com>
2020-04-01 21:33:19 +0200

    animation prototype

--- display_v.py
+++ display_v.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 import vehicle
 
+from matplotlib.animation import FuncAnimation
+
 
 def display_trajectory(car, input_series, starting_pos=[0, 0], total_time=100,  t_s=1):
     all_x, all_y = car.get_trajectory(input_series, starting_pos, total_time, t_s)
@@ -9,6 +11,10 @@ def display_trajectory(car, input_series, starting_pos=[0, 0], total_time=100,  
     plt.plot(all_x, all_y, '.')
     plt.show()
 
+def get_car_trajectory(car, input_series, starting_pos, total_time, t_s):
+    return car.get_trajectory(input_series, starting_pos, total_time, t_s) 
+
+# DEMO
 
 car = vehicle.Car()
 
@@ -17,4 +23,18 @@ timestep = 1
 
 inputs_file = 'inputs.csv'
 
-display_trajectory(car, inputs_file, [0, 0], total_time, timestep)
+#display_trajectory(car, inputs_file, [0, 0], total_time, timestep)
+
+fig = plt.figure()
+plt.axes(aspect='equal')
+ax1 = plt.subplot(1,1,1)
+
+
+all_x, all_y = get_car_trajectory(car, inputs_file, [0, 0], total_time, timestep)
+
+
+def animate(i):
+    ax1.plot(all_x.pop(0), all_y.pop(0), '.')
+
+ani = FuncAnimation(fig, animate, frames=total_time, interval = timestep * 1000)
+plt.show()
