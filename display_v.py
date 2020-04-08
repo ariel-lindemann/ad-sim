9710f92405fb16eb6e855652c0a35964c760cbc8
Ariel Lindemann <ariel.lindemann@gmail.com>
2020-04-08 22:08:58 +0200

    refractoring

--- display_v.py
+++ display_v.py
@@ -4,37 +4,45 @@ import vehicle
 from matplotlib.animation import FuncAnimation
 
 
-def display_trajectory(car, input_series, starting_pos=[0, 0], total_time=100,  t_s=1):
-    all_x, all_y = car.get_trajectory(input_series, starting_pos, total_time, t_s)
+def display_trajectory(car, input_series, starting_pos=[0, 0],
+                       TOTAL_TIME=100,  t_s=1):
+    all_x, all_y = car.get_trajectory(input_series, starting_pos,
+                                      TOTAL_TIME, t_s)
     plt.axes(aspect='equal')
 
     plt.plot(all_x, all_y, '.')
     plt.show()
 
-def get_car_trajectory(car, input_series, starting_pos, total_time, t_s):
-    return car.get_trajectory(input_series, starting_pos, total_time, t_s) 
+
+def run_sim(car, options, inputs):
+
+    TOTAL_TIME = options['TOTAL_TIME']
+    TIMESTEP = options['TIMESTEP']
+
+    fig = plt.figure()
+    plt.axes(aspect='equal')
+    ax1 = plt.subplot(1, 1, 1)
+
+    all_x, all_y = car.get_trajectory(inputs_file, [0, 0],
+                                      TOTAL_TIME, TIMESTEP)
+
+    def animate(i):
+        ax1.plot(all_x.pop(0), all_y.pop(0), '.')
+
+    ani = FuncAnimation(fig, animate, frames=TOTAL_TIME,
+                        interval=TIMESTEP * 1000)
+    plt.show()
+
 
 # DEMO
 
 car = vehicle.Car()
 
-total_time = 100
-timestep = 1
+options = {}
+options['TOTAL_TIME'] = 100
+options['TIMESTEP'] = 1
 
 inputs_file = 'inputs.csv'
 
-#display_trajectory(car, inputs_file, [0, 0], total_time, timestep)
-
-fig = plt.figure()
-plt.axes(aspect='equal')
-ax1 = plt.subplot(1,1,1)
-
-
-all_x, all_y = get_car_trajectory(car, inputs_file, [0, 0], total_time, timestep)
-
-
-def animate(i):
-    ax1.plot(all_x.pop(0), all_y.pop(0), '.')
-
-ani = FuncAnimation(fig, animate, frames=total_time, interval = timestep * 1000)
-plt.show()
+#display_trajectory(car, inputs_file, [0, 0], TOTAL_TIME, TIMESTEP)
+run_sim(car, options, inputs_file)
